9f98047c1c4fa79e12829890ac05aeb89f0019cc
kamalesh-pathy-va <asokankamalesh@gmail.com>
2023-02-06 12:09:52 +0530

    handle exceptions, add comments

--- hand_tracker.ino
+++ hand_tracker.ino
@@ -9,7 +9,11 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) {
     int angle = Serial.parseInt();
-    if (angle > 0)
+    // parseInt will wait for sometime to see if there's any number in the buffer.
+    // ending with a non-numarical value will return quickly, but converts the
+    // non-numarical value to 0
+    if (angle > 0) // so except for angle 0 rest of the angle it should turn the servo
+    // this will prevent the servo resetting to 0
       myservo.write(angle);
   }
 }
--- main.py
+++ main.py
@@ -1,20 +1,32 @@
+'''
+References from which I copied the code :)
+https://www.section.io/engineering-education/creating-a-hand-tracking-module/
+https://google.github.io/mediapipe/solutions/hands
+
+appart from that a special thanks to Chat GPT.
+
+See the Arduino code side by side for better understanding.
+'''
+
 import cv2
 import mediapipe as mp
 import serial
 import numpy as np
-import time
 
 cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
-ser = serial.Serial('/dev/ttyACM0', 115200)
+ser = serial.Serial('/dev/ttyACM0', 115200) # set serial port and baud rate
 
 try:
 	while True:
 		success, image = cap.read()
-		img = np.zeros((480, 640, 3), dtype = np.uint8)
+		img = np.zeros((480, 640, 3), dtype = np.uint8) # Black output screen
+		# if want to see the actual video output change `img` to `image' @ the following
+		# places #1, #2, #3
+
 		imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 		results = hands.process(imageRGB)
 
@@ -23,16 +35,18 @@ try:
 				for id, lm in enumerate(handLms.landmark):
 					h, w, c = image.shape
 					cx, cy = int(lm.x * w), int(lm.y *h)
-					if id == 9:
-						cv2.circle(img, (cx, cy), 10, (0, 255, 120), cv2.FILLED)
-						print(cx, end=" ")
-						deg = int(0.287*cx - 1.87)
-						print(deg)
-						if deg > 0:
-							ser.write(bytes(str(deg)+'\0', 'utf-8'))
-				mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+					if id == 9: # This point will be located at the center of your hand
+						cv2.circle(img, (cx, cy), 10, (0, 255, 120), cv2.FILLED) #1
+						print("Tracking point:", cx, end=" ")
+						deg = int(0.287*cx - 1.87) # converting the x-axis values to respective angle
+						print("Angle:",deg)
+						if deg > 0: # to neglect -ve angle after convertion
+							ser.write(bytes(str(deg)+'\0', 'utf-8')) # sending bytes, end with a non-numarical value
+				mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS) #2
 
-		cv2.imshow("Output", img)
+		cv2.imshow("Output", img) #3
 		cv2.waitKey(1)
+
 except KeyboardInterrupt:
+	cv2.destroyAllWindows()
 	exit(0)
